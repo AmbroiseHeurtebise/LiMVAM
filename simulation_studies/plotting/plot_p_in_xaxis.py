593900a3b70b7ab636aae49d8e4c2ada86f9c9b8
AmbroiseHeurtebise <amheurtebise@gmail.com>
2025-05-13 23:23:00 +0200

    cosmetic changes

--- simulation_studies/plotting/plot_p_in_xaxis.py
+++ simulation_studies/plotting/plot_p_in_xaxis.py
@@ -21,15 +21,14 @@ plt.rcParams.update(rc)
 # parameters 
 nb_seeds = 20
 metric = "error_B"  # or "error_T", "error_P_exact", "error_P_spearmanr", "amari_distance"
-include_multiviewica = False
 beta1 = 1.5
 beta2 = 2.5
 
 # read dataframe
 beta1_str = str(beta1).replace('.', '')
 beta2_str = str(beta2).replace('.', '')
-results_dir = "/storage/store2/work/aheurteb/MICaDo/simulation_studies/results/results_p_in_xaxis/"
-save_name = f"DataFrame_with_{nb_seeds}_seeds_beta_{beta1_str}_{beta2_str}"
+results_dir = "/storage/store2/work/aheurteb/LiMVAM/simulation_studies/results/results_p_in_xaxis/"
+save_name = f"DataFrame_with_{nb_seeds}_seeds_beta_{beta1_str}_{beta2_str}_time_and_scale"
 save_path = results_dir + save_name
 df = pd.read_csv(save_path)
 
@@ -50,17 +49,11 @@ elif metric == "amari_distance":
     metric_name = "Amari distance"
 
 # labels, dashes and curves order
-labels = ['MICaDo-ML', 'MICaDo-J', 'ICA-LiNGAM', 'MultiGroupDirectLiNGAM']
-dashes = ['', '', (2, 2), (2, 2)]
-hue_order = ["shica_ml", "shica_j", "lingam", "multi_group_direct_lingam"]
-if include_multiviewica:
-    labels.append('MICaDo-MVICA')
-    dashes.append('')
-    hue_order.append("multiviewica")
-    filtered_df = df
-else:
-    # remove MVICA LiNGAM curve
-    filtered_df = df[df["ica_algo"] != "multiviewica"]
+labels = ['PairwiseLiMVAM', 'MICaDo-ML', 'MICaDo-J', 'ICA-LiNGAM', 'MultiGroupDirectLiNGAM']
+dashes = ['', (2, 2), (2, 2), (2, 2), (2, 2)]
+hue_order = ["pairwise", "shica_ml", "shica_j", "lingam", "multi_group_direct_lingam"]
+# remove MVICA LiNGAM and MV-NOTEARS curves
+filtered_df = df[(df["ica_algo"] != "multiviewica") & (df["ica_algo"] != "mv_notears")]
 
 # subplots
 fig, axes = plt.subplots(2, 3, figsize=(12, 4.8), sharex=True, sharey=True)
@@ -82,44 +75,41 @@ ylabel = fig.supylabel(metric_name, fontsize=fontsize)
 xlabel.set_position((0.5, 0.07))
 ylabel.set_position((0.032, 0.5))
 ax.set_xticks(p_list)
-if include_multiviewica:
-    ymin, ymax = ax.get_ylim()
-    ax.set_ylim(ymin, 1e2)
+# if include_multiviewica:
+#     ymin, ymax = ax.get_ylim()
+#     ax.set_ylim(ymin, 1e2)
 plt.tight_layout()
 plt.subplots_adjust(hspace=0.3, wspace=0.1)
 
 # legend
-palette = sns.color_palette()[:5]
+palette = sns.color_palette()
 legend_styles = [
     Line2D([0], [0], color=palette[0], linewidth=2.5, linestyle='-', marker='o', 
            markeredgecolor="white", markersize=6),
-    Line2D([0], [0], color=palette[1], linewidth=2.5, linestyle='-', marker='X', 
+    Line2D([0], [0], color=palette[1], linewidth=2.5, linestyle='--', marker='X', 
            markeredgecolor="white", markersize=7),
     Line2D([0], [0], color=palette[2], linewidth=2.5, linestyle='--', marker='s', 
            markeredgecolor="white", markersize=5),
     Line2D([0], [0], color=palette[3], linewidth=2.5, linestyle='--', marker='P', 
            markeredgecolor="white", markersize=6),
+    Line2D([0], [0], color=palette[4], linewidth=2.5, linestyle='--', marker='D', 
+           markeredgecolor="white", markersize=5),
 ]
-if include_multiviewica:
-    legend_styles.append(
-        Line2D([0], [0], color=palette[4], linewidth=2.5, linestyle='-', marker='D', 
-               markeredgecolor="white", markersize=5))
-ncol = 3 if include_multiviewica else 4
-y_leg = 1.05 if include_multiviewica else 1.02
+ncol = 3
 fig.legend(
-    legend_styles, labels, bbox_to_anchor=(0.5, y_leg), loc="center",
+    legend_styles, labels, bbox_to_anchor=(0.5, 1.05), loc="center",
     ncol=ncol, borderaxespad=0., fontsize=fontsize
 )
 
-# caption
-caption = (
-    "Caption: Separation performance of ICA-LiNGAM, MultiGroupDirectLiNGAM, and two versions \n"
-    "of our method when we vary the number of views $m$ and disturbances $p$. The disturbances are \n"
-    "evenly divided into sub-Gaussian, Gaussian, and super-Gaussian groups."
-)
-fig.text(0.5, -0.05, caption, ha='center', va='center', fontsize=fontsize)
+# # caption
+# caption = (
+#     "Caption: Separation performance of ICA-LiNGAM, MultiGroupDirectLiNGAM, and two versions \n"
+#     "of our method when we vary the number of views $m$ and disturbances $p$. The disturbances are \n"
+#     "evenly divided into sub-Gaussian, Gaussian, and super-Gaussian groups."
+# )
+# fig.text(0.5, -0.05, caption, ha='center', va='center', fontsize=fontsize)
 
 # save figure
-figures_dir = Path("/storage/store2/work/aheurteb/MICaDo/simulation_studies/figures")
+figures_dir = Path("/storage/store2/work/aheurteb/LiMVAM/simulation_studies/figures")
 plt.savefig(figures_dir / f"simulation_p_in_xaxis.pdf", bbox_inches="tight")
 plt.show()

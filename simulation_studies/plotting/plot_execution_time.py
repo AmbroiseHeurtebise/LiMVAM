a8e79e8db6202cb580f6f12d63abfb1cec8c6c4a
AmbroiseHeurtebise <amheurtebise@gmail.com>
2025-05-14 02:59:42 +0200

    cosmetic changes

--- simulation_studies/plotting/plot_execution_time.py
+++ simulation_studies/plotting/plot_execution_time.py
@@ -13,40 +13,41 @@ rc = {
     "ytick.labelsize": fontsize,
     "font.family": "serif",
 }
-sns.set(style="white")  # "whitegrid"
+sns.set(style="white")
 plt.rcParams.update(rc)
 
 # parameters 
-nb_seeds = 50
+nb_seeds = 200
 
 # read dataframe
 simulation_dir = Path("/storage/store2/work/aheurteb/LiMVAM/simulation_studies")
-results_dir = simulation_dir / "results/results_timepoints_in_xaxis/shared_P"
-save_name = f"DataFrame_with_{nb_seeds}_seeds_and_time"
+results_dir = simulation_dir / "results/results_execution_time"
+save_name = f"DataFrame_with_{nb_seeds}_seeds_time_and_scale"
 save_path = results_dir / save_name
 df = pd.read_csv(save_path)
 
-# Remove MICaDo-MVICA curve, and keep only data for n=1000 and nb_gaussian_disturbances=2
-df_filtered = df[
-    (df["n"] == 1000) 
-    & (df["nb_gaussian_disturbances"] == 2) 
-    & (df["ica_algo"] != "multiviewica")]
-
-# Use color palette and loop through methods
+# Use color palette and markers
 palette_sns = sns.color_palette()
 palette = {
-    'shica_ml': palette_sns[0],
-    'shica_j': palette_sns[1],
-    'lingam': palette_sns[2],
-    'multi_group_direct_lingam': palette_sns[3],
-    'pairwise': palette_sns[4],
-    'mv_notears': palette_sns[5],
+    'pairwise': palette_sns[0],
+    'shica_ml': palette_sns[1],
+    'shica_j': palette_sns[2],
+    'lingam': palette_sns[3],
+    'multi_group_direct_lingam': palette_sns[4],
+}
+marker_styles = {
+    'pairwise': 'o',
+    'shica_ml': 'X',
+    'shica_j': 's',
+    'lingam': 'P',
+    'multi_group_direct_lingam': 'D',
 }
 
 # Create the plot
 fig, ax = plt.subplots(figsize=(6, 3.2))
 
-for method, group_df in df_filtered.groupby("ica_algo"):
+for method, group_df in df.groupby("ica_algo"):
+    # cloud of points
     sns.scatterplot(
         data=group_df,
         x="error_B",
@@ -55,28 +56,56 @@ for method, group_df in df_filtered.groupby("ica_algo"):
         alpha=0.2,
         label=None
     )
-    sns.lineplot(
-        data=group_df,
-        x="error_B",
-        y="execution_time",
+    
+    # median
+    x_median = group_df["error_B"].median()
+    y_median = group_df["execution_time"].median()
+    
+    # error bars
+    x_low = group_df["error_B"].quantile(0.16)
+    x_high = group_df["error_B"].quantile(0.84)
+    y_low = group_df["execution_time"].quantile(0.16)
+    y_high = group_df["execution_time"].quantile(0.84)
+    xerr = [[x_median - x_low], [x_high - x_median]]
+    yerr = [[y_median - y_low], [y_high - y_median]]
+    
+    # markersize
+    if method == "shica_j":
+        markersize = 9
+    elif method == "multi_group_direct_lingam":
+        markersize = 8
+    else:
+        markersize = 10
+
+    # markers and error bars
+    ax.errorbar(
+        x_median,
+        y_median,
+        xerr=xerr,
+        yerr=yerr,
+        fmt=marker_styles.get(method),
+        markersize=markersize,
         color=palette.get(method, "gray"),
-        label=None,
-        errorbar=None,
-        estimator="median",
-        sort=True,
-        linewidth=2
+        capsize=5,
+        elinewidth=2,
+        markerfacecolor=palette.get(method, "gray"),
+        markeredgecolor='white'
     )
 
 # legend
 legend_styles = [
-    Line2D([0], [0], color=palette_sns[0], linewidth=2.5, linestyle='-'),
-    Line2D([0], [0], color=palette_sns[1], linewidth=2.5, linestyle='-'),
-    Line2D([0], [0], color=palette_sns[2], linewidth=2.5, linestyle='--'),
-    Line2D([0], [0], color=palette_sns[3], linewidth=2.5, linestyle='--'),
-    Line2D([0], [0], color=palette_sns[4], linewidth=2.5, linestyle='-'),
-    Line2D([0], [0], color=palette_sns[5], linewidth=2.5, linestyle='-'),
+    Line2D([0], [0], marker='o', color='w',
+           markerfacecolor=palette_sns[0], markeredgecolor='white', markersize=10),
+    Line2D([0], [0], marker='X', color='w',
+           markerfacecolor=palette_sns[1], markeredgecolor='white', markersize=10),
+    Line2D([0], [0], marker='s', color='w',
+           markerfacecolor=palette_sns[2], markeredgecolor='white', markersize=9),
+    Line2D([0], [0], marker='P', color='w',
+           markerfacecolor=palette_sns[3], markeredgecolor='white', markersize=10),
+    Line2D([0], [0], marker='D', color='w',
+           markerfacecolor=palette_sns[4], markeredgecolor='white', markersize=8),
 ]
-labels = ['MICaDo-ML', 'MICaDo-J', 'ICA-LiNGAM', 'MultiGroupDirectLiNGAM', 'PairwiseLiMVAM', 'MV-NOTEARS']
+labels = ['PRaLiNE', 'MICaDo-ML', 'MICaDo-J', 'ICA-LiNGAM', 'MultiGroupDirectLiNGAM']
 fig.legend(
     legend_styles, labels, bbox_to_anchor=(0.5, 1.12), loc="center",
     ncol=2, borderaxespad=0., fontsize=fontsize
@@ -84,6 +113,7 @@ fig.legend(
 
 plt.xscale("log")
 plt.yscale("log")
+ax.set_xlim([10**(-4.1), 10**0.1])
 plt.xlabel(r"Error on $B^i$", fontsize=fontsize, family="serif")
 plt.ylabel("Fitting time (in s)", fontsize=fontsize, family="serif")
 ax.tick_params(which='major', bottom=True, left=True, length=4, width=0.8, color='black')

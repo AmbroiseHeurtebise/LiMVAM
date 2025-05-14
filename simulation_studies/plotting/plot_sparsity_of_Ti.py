99487b590123b3aa31f344b4c9d9a5e4245f0be1
AmbroiseHeurtebise <amheurtebise@gmail.com>
2025-05-14 04:26:29 +0200

    include pairwise

--- simulation_studies/plotting/plot_sparsity_of_Ti.py
+++ simulation_studies/plotting/plot_sparsity_of_Ti.py
@@ -19,12 +19,12 @@ rc = {
 plt.rcParams.update(rc)
 
 # parameters 
-nb_seeds = 50
+nb_seeds = 2
 metric = "error_P_spearmanr"  # or "error_B", "error_P_exact", "amari_distance"
 
 # read dataframe
-results_dir = "/storage/store2/work/aheurteb/MICaDo/simulation_studies/results/results_sparsity_of_Ti/"
-save_name = f"DataFrame_with_{nb_seeds}_seeds"
+results_dir = "/storage/store2/work/aheurteb/LiMVAM/simulation_studies/results/results_sparsity_of_Ti/"
+save_name = f"DataFrame_with_{nb_seeds}_seeds_time_and_scale"
 save_path = results_dir + save_name
 df = pd.read_csv(save_path)
 
@@ -41,33 +41,51 @@ elif metric == "amari_distance":
     metric_name = "Amari distance"
 
 # labels, dashes, curves order and titles
-labels = ['MICaDo-ML']
-dashes = ['']
-hue_order = ["shica_ml"]
-titles = ["Multiple causal orderings", "Shared causal ordering"]
+labels = ['PRaLiNE', 'MICaDo-ML']
+hue_order = ["pairwise", "shica_ml"]
+
+# Use color palette
+palette_sns = sns.color_palette()
+palette = {
+    'pairwise': palette_sns[0],
+    'shica_ml': palette_sns[1],
+    'shica_j': palette_sns[2],
+}
 
 # subplots
 fig, axes = plt.subplots(1, 2, figsize=(8, 2.8), sharex=True, sharey=True)
-for i, ax in enumerate(axes.flat):
-    shared_causal_ordering = bool(i % 2)
-    data = df[df["shared_causal_ordering"] == shared_causal_ordering]
-    sns.lineplot(
-        data=data, x="nb_zeros_Ti", y=metric, linewidth=2.5, hue="ica_algo", estimator=np.median,
-        errorbar=('ci', 95), hue_order=hue_order, style_order=hue_order, style="ica_algo", ax=ax,
-        dashes=dashes, markers=True)
-    ax.set_xlabel("")
-    if i == 0:
-        ylabel = ax.set_ylabel(metric_name)
-        ax.yaxis.set_label_coords(-0.12, 0.5)
-    ax.set_title(titles[i], fontsize=fontsize)
-    ax.grid(which='both', linewidth=0.5, alpha=0.5)
-    ax.get_legend().remove()
+
+# first subplot
+data1 = df[(df["shared_causal_ordering"] == 0) & (df["ica_algo"] == "shica_ml")]
+sns.lineplot(
+    data=data1, x="nb_zeros_Ti", y=metric, linewidth=2.5,
+    hue="ica_algo", style="ica_algo", style_order=["shica_ml"],
+    dashes={"shica_ml": (2, 2)}, markers={"shica_ml": "X"}, palette={"shica_ml": palette_sns[1]},
+    estimator=np.median, errorbar=('ci', 95), ax=axes[0])
+axes[0].set_xlabel("")
+ylabel = axes[0].set_ylabel(metric_name)
+axes[0].yaxis.set_label_coords(-0.12, 0.5)
+axes[0].set_title("Multiple causal orderings", fontsize=fontsize)
+axes[0].grid(which='both', linewidth=0.5, alpha=0.5)
+axes[0].get_legend().remove()
+
+# second subplot
+data2 = df[df["shared_causal_ordering"] == 1]
+sns.lineplot(
+    data=data2, x="nb_zeros_Ti", y=metric, linewidth=2.5, hue="ica_algo", estimator=np.median,
+    errorbar=('ci', 95), ax=axes[1], hue_order=hue_order, style_order=hue_order, style="ica_algo",
+    dashes=['', (2, 2)], markers=True)
+axes[1].set_xlabel("")
+axes[1].set_title("Shared causal ordering", fontsize=fontsize)
+axes[1].grid(which='both', linewidth=0.5, alpha=0.5)
+axes[1].get_legend().remove()
+axes[1].set_yticks([0, 1])
+axes[1].set_yticklabels([0, 1])
+
 # ax.set_ylim(-1.1, 1.1)
 # ax.set_yticks([-1, 0, 1])
 # ax.set_yticklabels([-1, 0, 1])
-ax.set_yticks([0, 1])
-ax.set_yticklabels([0, 1])
-xlabel = fig.supxlabel(r"# sparse entries in each $T^i$", fontsize=fontsize)
+xlabel = fig.supxlabel(r"Number of sparse entries in each $T^i$", fontsize=fontsize)
 xlabel.set_position((0.5, 0.16))
 plt.tight_layout()
 plt.subplots_adjust(hspace=0.15)
@@ -77,7 +95,7 @@ palette = sns.color_palette()[:2]
 legend_styles = [
     Line2D([0], [0], color=palette[0], linewidth=2.5, linestyle='-', marker='o', 
            markeredgecolor="white", markersize=6),
-    Line2D([0], [0], color=palette[1], linewidth=2.5, linestyle='-', marker='X', 
+    Line2D([0], [0], color=palette[1], linewidth=2.5, linestyle='--', marker='X', 
            markeredgecolor="white", markersize=7),
 ]
 fig.legend(
@@ -85,19 +103,19 @@ fig.legend(
     ncol=2, borderaxespad=0., fontsize=fontsize
 )
 
-# caption
-caption = (
-    "Caption: Data are generated with $m=8$ views and $p=6$ disturbances, consisting \n"
-    "of 2 sub-Gaussian, 2 Gaussian, and 2 super-Gaussian disturbances. The x-axis \n"
-    "represents the number of sparse entries in the strictly lower triangular part of \n"
-    "each " + r"$T^i$" + ", while the y-axis shows the Spearman's rank correlation between true and \n"
-    "estimated causal orderings. Results justify Assumptions 2 and 3, since recovering \n"
-    "the causal ordering is significantly easier in the shared causal ordering scenario \n"
-    "(Assumption 3) than in the multiple causal orderings scenario (Assumption 2)."
-)
-fig.text(0.5, -0.2, caption, ha='center', va='center', fontsize=fontsize)
+# # caption
+# caption = (
+#     "Caption: Data are generated with $m=8$ views and $p=6$ disturbances, consisting \n"
+#     "of 2 sub-Gaussian, 2 Gaussian, and 2 super-Gaussian disturbances. The x-axis \n"
+#     "represents the number of sparse entries in the strictly lower triangular part of \n"
+#     "each " + r"$T^i$" + ", while the y-axis shows the Spearman's rank correlation between true and \n"
+#     "estimated causal orderings. Results justify Assumptions 2 and 3, since recovering \n"
+#     "the causal ordering is significantly easier in the shared causal ordering scenario \n"
+#     "(Assumption 3) than in the multiple causal orderings scenario (Assumption 2)."
+# )
+# fig.text(0.5, -0.2, caption, ha='center', va='center', fontsize=fontsize)
 
 # save figure
-figures_dir = Path("/storage/store2/work/aheurteb/MICaDo/simulation_studies/figures")
+figures_dir = Path("/storage/store2/work/aheurteb/LiMVAM/simulation_studies/figures")
 plt.savefig(figures_dir / f"simulation_sparsity_of_Ti.pdf", bbox_inches="tight")
 plt.show()
